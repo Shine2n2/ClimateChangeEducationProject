4af00d36c463d598dc47326d41fa67b95ce6a650
Shine2n2 <kelikume@gmail.com>
2023-06-06 07:31:20 +0100

    local storage setup

---
+++ ClimateChangeEducation.API/ClimateChangeEducation.Infrastructure/Repositories/LocalImageStorageRepository.cs
@@ -0,0 +1,26 @@
+﻿using ClimateChangeEducation.Infrastructure.Interfaces;
+using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClimateChangeEducation.Infrastructure.Repositories
+{
+    public class LocalImageStorageRepository: ILocalImageStorageRepository
+    {
+        public async Task<string> Upload(IFormFile file, string fileName)
+        {
+            var filePath = Path.Combine(Directory.GetCurrentDirectory(), @"Resources\Images", fileName);
+            using Stream fileStream = new FileStream(filePath, FileMode.Create);
+            await file.CopyToAsync(fileStream);
+            return GetServerRelativePath(fileName);
+        }
+
+        private string GetServerRelativePath(string fileName)
+        {
+            return Path.Combine(@"Resources\Images", fileName);
+        }
+    }
+}
