512ea0f7c574b43f37fd95edbe236f1a0e125363
Shine2n2 <kelikume@gmail.com>
2023-06-08 10:20:16 +0100

    updated

--- ClimateChangeEducation.API/ClimateChangeEducation.API/Controllers/CourseController.cs
+++ ClimateChangeEducation.API/ClimateChangeEducation.API/Controllers/CourseController.cs
@@ -36,27 +36,74 @@ namespace ClimateChangeEducation.API.Controllers
 
         // GET api/<CourseController>/5
         [HttpGet("{id}")]
-        public string Get(int id)
+        public async Task<IActionResult> GetCourseById([FromRoute]string id)
         {
-            return "value";
+            try
+            {
+                var result = await _courseRepo.GetCourseByIdAsync(id);
+                return Ok(_mapper.Map<Course>(result));
+            }
+            catch (ArgumentException argex)
+            {
+                return BadRequest(argex.Message);
+            }
         }
 
         // POST api/<CourseController>
         [HttpPost]
-        public void Post([FromBody] string value)
+        public async Task<IActionResult> CreateCourse([FromBody] Course request)
         {
+            try
+            {
+                var course = await _courseRepo.CreateCourseAsync(_mapper.Map<Course>(request));
+                return Ok(course);
+            }
+            catch (ArgumentException argex)
+            {
+                return BadRequest(argex.Message);
+            }
         }
 
         // PUT api/<CourseController>/5
         [HttpPut("{id}")]
-        public void Put(int id, [FromBody] string value)
+        public async Task<IActionResult> UpdateCourse([FromRoute] string id, [FromBody] Course request)
         {
+            try
+            {
+                if (await _courseRepo.ExistsCourseAsync(id))
+                {
+                    // Update Details
+                    var updatedCourse = await _courseRepo.UpdateCourseAsync(id, _mapper.Map<Course>(request));
+                    if (updatedCourse != null)
+                    {
+                        return Ok(_mapper.Map<Student>(updatedCourse));
+                    }
+                }
+                return NotFound();
+            }
+            catch (ArgumentException argex)
+            {
+                return BadRequest(argex.Message);
+            }
         }
 
         // DELETE api/<CourseController>/5
         [HttpDelete("{id}")]
-        public void Delete(int id)
+        public async Task<IActionResult> DeleteCourse([FromRoute] string id)
         {
+            try
+            {
+                if (await _courseRepo.ExistsCourseAsync(id))
+                {
+                    var course = await _courseRepo.DeleteCourse(id);
+                    return Ok(_mapper.Map<Course>(course));
+                }
+                return NotFound();
+            }
+            catch (ArgumentException argex)
+            {
+                return BadRequest(argex.Message);
+            }
         }
     }
 }
--- ClimateChangeEducation.API/ClimateChangeEducation.API/Controllers/DiscussionBoardController.cs
+++ ClimateChangeEducation.API/ClimateChangeEducation.API/Controllers/DiscussionBoardController.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using AutoMapper;
+using ClimateChangeEducation.Domain.Entities;
+using ClimateChangeEducation.Infrastructure.Interfaces;
+using Microsoft.AspNetCore.Mvc;
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -8,36 +11,99 @@ namespace ClimateChangeEducation.API.Controllers
     [ApiController]
     public class DiscussionBoardController : ControllerBase
     {
+        private readonly IDiscussionBoardRepository _discussionBoardRepo;
+        private readonly IMapper _mapper;
+
+        public DiscussionBoardController(IDiscussionBoardRepository discussionBoardRepo, IMapper mapper)
+        {
+            _discussionBoardRepo = discussionBoardRepo;
+            _mapper = mapper;
+        }
         // GET: api/<DiscussionBoardController>
         [HttpGet]
-        public IEnumerable<string> Get()
+        public async Task<IActionResult> GetDiscussionBoards()
         {
-            return new string[] { "value1", "value2" };
+            try
+            {
+                var discussionboards = await _discussionBoardRepo.GetAllDiscussionBoardAsync();
+                return (Ok(_mapper.Map<List<DiscussionBoard>>(discussionboards)));
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
         }
 
         // GET api/<DiscussionBoardController>/5
         [HttpGet("{id}")]
-        public string Get(int id)
+        public async Task<IActionResult> GetDiscussionBoardById([FromRoute] string id)
         {
-            return "value";
+            try
+            {
+                var result = await _discussionBoardRepo.GetDiscussionBoardByIdAsync(id);
+                return Ok(_mapper.Map<DiscussionBoard>(result));
+            }
+            catch (ArgumentException argex)
+            {
+                return BadRequest(argex.Message);
+            }
         }
 
         // POST api/<DiscussionBoardController>
         [HttpPost]
-        public void Post([FromBody] string value)
+        public async Task<IActionResult> CreateDiscussionBoard([FromBody] DiscussionBoard request)
         {
+            try
+            {
+                var discussionBoard = await _discussionBoardRepo.CreateDiscussionBoardAsync(_mapper.Map<DiscussionBoard>(request));
+                return Ok(discussionBoard);
+            }
+            catch (ArgumentException argex)
+            {
+                return BadRequest(argex.Message);
+            }
         }
 
         // PUT api/<DiscussionBoardController>/5
         [HttpPut("{id}")]
-        public void Put(int id, [FromBody] string value)
+        public async Task<IActionResult> UpdateDiscussionBoard([FromRoute] string id, [FromBody] Course request)
         {
+            try
+            {
+                if (await _discussionBoardRepo.ExistsDiscussionBoardAsync(id))
+                {
+                    // Update Details
+                    var updated = await _discussionBoardRepo.UpdateDiscussionBoardAsync(id, _mapper.Map<DiscussionBoard>(request));
+                    if (updated != null)
+                    {
+                        return Ok(_mapper.Map<Student>(updated));
+                    }
+                }
+                return NotFound();
+            }
+            catch (ArgumentException argex)
+            {
+                return BadRequest(argex.Message);
+            }
         }
 
         // DELETE api/<DiscussionBoardController>/5
         [HttpDelete("{id}")]
-        public void Delete(int id)
+        public async Task<IActionResult> DeleteDiscussionBoard([FromRoute] string id)
         {
+            try
+            {
+                if (await _discussionBoardRepo.ExistsDiscussionBoardAsync(id))
+                {
+                    var result = await _discussionBoardRepo.DeleteDiscussionBoard(id);
+                    return Ok(_mapper.Map<Course>(result));
+                }
+                return NotFound();
+            }
+            catch (ArgumentException argex)
+            {
+                return BadRequest(argex.Message);
+            }
         }
     }
 }
