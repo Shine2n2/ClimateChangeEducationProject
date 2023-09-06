cd18340a8607b2618e907f6964d1cd6a8b760df8
Shine2n2 <kelikume@gmail.com>
2023-09-06 10:20:32 +0100

    update

--- ClimateChangeEducation.API/ClimateChangeEducation.Infrastructure/Repositories/LocalImageStorageRepository.cs
+++ ClimateChangeEducation.API/ClimateChangeEducation.Infrastructure/Repositories/LocalImageStorageRepository.cs
@@ -17,7 +17,8 @@ namespace ClimateChangeEducation.Infrastructure.Repositories
 
         public async Task<string> UploadImg(IFormFile file, string fileName)
         {
-            var filePath = Path.Combine(Directory.GetCurrentDirectory(), @"C:\Users\Decagon\Desktop\ezimoha\PROJECT\Backend\ClimateChangeEducationProject\ClimateChangeEducation.API\ClimateChangeEducation.Infrastructure\Resources\Images\", fileName);
+           // var filePath = Path.Combine(Directory.GetCurrentDirectory(), @"C:\Users\Decagon\Desktop\ezimoha\PROJECT\Backend\ClimateChangeEducationProject\ClimateChangeEducation.API\ClimateChangeEducation.Infrastructure\Resources\Images\", fileName);
+            var filePath = Path.Combine(Directory.GetCurrentDirectory(), @".\Resources\Images\", fileName);
             using Stream fileStream = new FileStream(filePath, FileMode.Create);
             await file.CopyToAsync(fileStream);
             return GetRelativePathforImage(fileName);
@@ -25,12 +26,14 @@ namespace ClimateChangeEducation.Infrastructure.Repositories
 
         private string GetRelativePathforImage(string fileName)
         {
-            return Path.Combine(@"C:\Users\Decagon\Desktop\ezimoha\PROJECT\Backend\ClimateChangeEducationProject\ClimateChangeEducation.API\ClimateChangeEducation.Infrastructure\Resources\Images\", fileName);
+            //return Path.Combine(@"C:\Users\Decagon\Desktop\ezimoha\PROJECT\Backend\ClimateChangeEducationProject\ClimateChangeEducation.API\ClimateChangeEducation.Infrastructure\Resources\Images\", fileName);
+            return Path.Combine(@".\Resources\Images\", fileName);
         }
 
         public async Task<string> UploadVideo(IFormFile file, string fileName)
         {
-            var filePath = Path.Combine(Directory.GetCurrentDirectory(), @"C:\Users\Decagon\Desktop\ezimoha\PROJECT\Backend\ClimateChangeEducationProject\ClimateChangeEducation.API\ClimateChangeEducation.Infrastructure\Resources\Videos\", fileName);
+            //var filePath = Path.Combine(Directory.GetCurrentDirectory(), @"C:\Users\Decagon\Desktop\ezimoha\PROJECT\Backend\ClimateChangeEducationProject\ClimateChangeEducation.API\ClimateChangeEducation.Infrastructure\Resources\Videos\", fileName);
+            var filePath = Path.Combine(Directory.GetCurrentDirectory(), @".\Resources\Videos\", fileName);
             using Stream fileStream = new FileStream(filePath, FileMode.Create);
             await file.CopyToAsync(fileStream);
             return GetRelativePathForVideo(fileName);
@@ -38,12 +41,14 @@ namespace ClimateChangeEducation.Infrastructure.Repositories
 
         private string GetRelativePathForVideo(string fileName)
         {
-            return Path.Combine(@"C:\Users\Decagon\Desktop\ezimoha\PROJECT\Backend\ClimateChangeEducationProject\ClimateChangeEducation.API\ClimateChangeEducation.Infrastructure\Resources\Videos\", fileName);
+            //return Path.Combine(@"C:\Users\Decagon\Desktop\ezimoha\PROJECT\Backend\ClimateChangeEducationProject\ClimateChangeEducation.API\ClimateChangeEducation.Infrastructure\Resources\Videos\", fileName);
+            return Path.Combine(@".\Resources\Videos\", fileName);
         }
 
         public async Task<string> UploadDocument(IFormFile file, string fileName)
         {
-            var filePath = Path.Combine(Directory.GetCurrentDirectory(), @"C:\Users\Decagon\Desktop\ezimoha\PROJECT\Backend\ClimateChangeEducationProject\ClimateChangeEducation.API\ClimateChangeEducation.Infrastructure\Resources\Documents\", fileName);
+            //var filePath = Path.Combine(Directory.GetCurrentDirectory(), @"C:\Users\Decagon\Desktop\ezimoha\PROJECT\Backend\ClimateChangeEducationProject\ClimateChangeEducation.API\ClimateChangeEducation.Infrastructure\Resources\Documents\", fileName);
+            var filePath = Path.Combine(Directory.GetCurrentDirectory(), @".\Resources\Documents\", fileName);
             using Stream fileStream = new FileStream(filePath, FileMode.Create);
             await file.CopyToAsync(fileStream);
             return GetRelativePathForDocument(fileName);
@@ -51,7 +56,8 @@ namespace ClimateChangeEducation.Infrastructure.Repositories
 
         private string GetRelativePathForDocument(string fileName)
         {
-            return Path.Combine(@"C:\Users\Decagon\Desktop\ezimoha\PROJECT\Backend\ClimateChangeEducationProject\ClimateChangeEducation.API\ClimateChangeEducation.Infrastructure\Resources\Documents\", fileName);
+            //return Path.Combine(@"C:\Users\Decagon\Desktop\ezimoha\PROJECT\Backend\ClimateChangeEducationProject\ClimateChangeEducation.API\ClimateChangeEducation.Infrastructure\Resources\Documents\", fileName);
+            return Path.Combine(@".\Resources\Documents\", fileName);
         }
 
 
