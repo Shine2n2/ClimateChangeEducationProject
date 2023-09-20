f0833b913a1189d5a75cf17ddde7e0009bb225a9
Shine2n2 <kelikume@gmail.com>
2023-09-20 16:06:50 +0100

    update

--- ClimateChangeEducation.API/ClimateChangeEducation.API/Program.cs
+++ ClimateChangeEducation.API/ClimateChangeEducation.API/Program.cs
@@ -152,7 +152,7 @@ builder.Services.AddEndpointsApiExplorer();
 
 builder.Services.AddSwaggerGen(c =>
 {
-    c.SwaggerDoc("v1", new OpenApiInfo { Title = "Your API", Version = "v1" });
+    c.SwaggerDoc("v1", new OpenApiInfo { Title = "Climate Edu Web API", Version = "v1" });
 
     // Define the security scheme for bearer tokens
     var securityScheme = new OpenApiSecurityScheme
---
+++ ClimateChangeEducation.API/ClimateChangeEducation.Domain/Entities/TestModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClimateChangeEducation.Domain.Entities
+{
+    public class TestModel
+    {
+        public int MyProperty { get; set; }
+    }
+}
